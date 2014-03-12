3c72d277af70c7b6331c50938b7f89cfba524e3d
Ben Constable <ben@benconstable.co.uk>
2014-03-12 12:53:07 +0000

    Remove functional testing junk

--- src/PhpSpec/Laravel/Util/Laravel.php
+++ src/PhpSpec/Laravel/Util/Laravel.php
@@ -20,13 +20,6 @@ class Laravel {
     public $app;
 
     /**
-     * The HttpKernel client instance.
-     *
-     * @var \Illuminate\Foundation\Testing\Client
-     */
-    public $client;
-
-    /**
      * The Laravel testing environment.
      *
      * @var string
@@ -66,62 +59,9 @@ class Laravel {
     {
         $this->app = $this->createApplication();
 
-        $this->client = $this->createClient($this->app);
-
         $this->app->setRequestForConsoleEnvironment();
 
         $this->app->boot();
-    }
-
-    /**
-     * Set the session to the given array.
-     *
-     * @param array $data
-     * @return void
-     */
-    public function session(array $data)
-    {
-        $this->startSession();
-
-        foreach ($data as $key => $value)
-        {
-            $this->app['session']->put($key, $value);
-        }
-    }
-
-    /**
-     * Flush all of the current session data.
-     *
-     * @return void
-     */
-    public function flushSession()
-    {
-        $this->startSession();
-
-        $this->app['session']->flush();
-    }
-
-    /**
-     * Set the currently logged in user for the application.
-     *
-     * @param \Illuminate\Auth\UserInterface $user
-     * @param string $driver
-     * @return void
-     */
-    public function be(UserInterface $user, $driver = null)
-    {
-        $this->app['auth']->driver($driver)->setUser($user);
-    }
-
-    /**
-     * Seed a given database connection.
-     *
-     * @param string $class
-     * @return void
-     */
-    public function seed($class = 'DatabaseSeeder')
-    {
-        $this->app['artisan']->call('db:seed', array('--class' => $class));
     }
 
     /**
@@ -136,29 +76,5 @@ class Laravel {
         $testEnvironment = $this->env;
 
         return require $this->bootstrapPath . '/start.php';
-    }
-
-    /**
-     * Create a new HttpKernel client instance.
-     *
-     * @param array $server
-     * @return \Symfony\Component\HttpKernel\Client
-     */
-    protected function createClient($app, $servers = array())
-    {
-        return new Client($app, $servers);
-    }
-
-    /**
-     * Start the session for the application.
-     *
-     * @return void
-     */
-    protected function startSession()
-    {
-        if (!$this->app['session']->isStarted())
-        {
-            $this->app['session']->start();
-        }
     }
 }
