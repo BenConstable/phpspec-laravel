58a2c9f2cd4dfdc9ea556bb5b500a77dbc888c08
Ben Constable <ben@benconstable.co.uk>
2014-04-19 13:39:30 +0100

    Improve API for Laravel util class and add tests

---
+++ spec/PhpSpec/Laravel/Util/LaravelSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\PhpSpec\Laravel\Util;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Illuminate\Foundation\Application;
+use Illuminate\Console\Application as Console;
+
+class LaravelSpec extends ObjectBehavior
+{
+    function let(Application $app)
+    {
+        $app->setRequestForConsoleEnvironment()->shouldBeCalled();
+        $app->boot()->shouldBeCalled();
+    }
+
+    function it_boots_in_the_testing_env_by_default(Application $app)
+    {
+        $this->beConstructedWith(null, '.', false, $app);
+        $this->getEnv()->shouldBe('testing');
+    }
+
+    function it_allows_the_env_to_be_set_to_anything(Application $app)
+    {
+        $this->beConstructedWith('whatever', '.', false, $app);
+        $this->getEnv()->shouldBe('whatever');
+    }
+
+    function it_will_run_migrations_if_told_to(Application $app, Console $console)
+    {
+        $console->call('migrate')->shouldBeCalled();
+        $app->make('artisan')->shouldBeCalled();
+        $app->make('artisan')->willReturn($console);
+
+        $this->beConstructedWith(null, '.', true, $app);
+        $this->getMigrateDatabase()->shouldBe(true);
+    }
+
+    function it_allows_access_to_the_app(Application $app)
+    {
+        $this->beConstructedWith(null, '.', false, $app);
+        $this->app->shouldHaveType('Illuminate\Foundation\Application');
+    }
+
+    function it_throws_an_exception_when_trying_to_get_inaccessible_vars(Application $app)
+    {
+        $this->beConstructedWith(null, '.', false, $app);
+        $this->shouldThrow('ErrorException')->during('__get', array('inaccessible'));
+    }
+}
--- src/PhpSpec/Laravel/Util/Laravel.php
+++ src/PhpSpec/Laravel/Util/Laravel.php
@@ -1,5 +1,8 @@
 <?php namespace PhpSpec\Laravel\Util;
 
+use ErrorException;
+use Illuminate\Foundation\Application;
+
 /**
  * This class provides an entry point into Laravel for PhpSpec.
  */
@@ -10,7 +13,7 @@ class Laravel {
      *
      * @var \Illuminate\Foundation\Application
      */
-    public $app;
+    private $app;
 
     /**
      * The Laravel testing environment.
@@ -38,42 +41,78 @@ class Laravel {
      *
      * Setup application on construct.
      *
-     * @param string $env           Laravel testing environment. 'testing' by default
-     * @param string $bootstrapPath Path to the Laravel bootstrap dir
-     * @param boolean                       $migrateDatabase
+     * @param string  $env             Laravel testing environment. 'testing' by default
+     * @param string  $bootstrapPath   Path to the Laravel bootstrap dir
+     * @param boolean $migrateDatabase Whether or not to run db migrations after
+     *                                 bootstrapping. False by default
+     * @param mixed                    @see refreshApplication()
      * @return void
      */
-    public function __construct($env, $bootstrapPath, $migrateDatabase = false)
+    public function __construct($env, $bootstrapPath, $migrateDatabase = false, $app = null)
     {
         $this->env = $env ?: 'testing';
         $this->bootstrapPath = $bootstrapPath;
         $this->migrateDatabase = $migrateDatabase;
 
-        $this->refreshApplication();
+        $this->refreshApplication($app);
     }
 
     /**
      * Refresh the application instance.
      *
+     * @param \Illuminate\Foundation\Application $app Optionally provide your own unbooted
+     *                                                Laravel Application instance. This
+     *                                                parameter can largely be ignored and
+     *                                                is used just for unit testing
      * @return void
      */
-    public function refreshApplication()
+    public function refreshApplication($app = null)
     {
-        $this->app = $this->createApplication();
+        $this->app = $app instanceof Application ? $app : $this->createApplication();
 
         $this->app->setRequestForConsoleEnvironment();
 
         $this->app->boot();
 
         if ($this->migrateDatabase) {
-            $this->app['artisan']->call('migrate');
+            $this->app->make('artisan')->call('migrate');
         }
     }
 
     /**
-     * Creates the application.
+     * Get the Laravel application environment being used.
      *
-     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+     * @return string Environment name
+     */
+    public function getEnv()
+    {
+        return $this->env;
+    }
+
+    /**
+     * Get the path to the Laravel bootstrap dir.
+     *
+     * @return string
+     */
+    public function getBootstrapPath()
+    {
+        return $this->bootstrapPath;
+    }
+
+    /**
+     * Get the database migration flag.
+     *
+     * @return boolean
+     */
+    public function getMigrateDatabase()
+    {
+        return $this->migrateDatabase;
+    }
+
+    /**
+     * Creates a Laravel application.
+     *
+     * @return \Illuminate\Foundation\Application
      */
     protected function createApplication()
     {
@@ -82,5 +121,24 @@ class Laravel {
         $testEnvironment = $this->env;
 
         return require $this->bootstrapPath . '/start.php';
+    }
+
+    /**
+     * Provide public access to the $app variable.
+     *
+     * Throw an exception if attempting to get anything else.
+     *
+     * @param  string          $name Name of variable to get
+     * @return mixed                 Variable
+     * @throws \ErrorException       If attempting to get something other than $app
+     */
+    public function __get($name)
+    {
+        if ($name === 'app') {
+            return $this->app;
+        }
+
+        throw new ErrorException(
+            "Attempting to get inaccessible or undefined property $name");
     }
 }
