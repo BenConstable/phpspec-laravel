9ea7a9dd7fb76226634e34e810b744319775ab36
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-10-27 01:12:34 -0500

    Use custom console kernel to get around exception
    when using "phpspec run"

---
+++ src/PhpSpec/Laravel/Util/ConsoleKernel.php
@@ -0,0 +1,41 @@
+<?php namespace PhpSpec\Laravel\Util;
+
+use Exception;
+use Illuminate\Foundation\Console\Kernel;
+
+/**
+ * Class ConsoleKernel
+ *
+ * @package PhpSpec\Laravel\Util
+ */
+class ConsoleKernel extends Kernel {
+
+    /**
+     * Run the console application.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    public function handle($input, $output = null)
+    {
+        /**
+         * We are probably using "phpspec run", don't throw an exception for that.
+         */
+        if ($input->getFirstArgument() == 'run') {
+            return 1;
+        }
+
+        try
+        {
+            return parent::handle($input, $output);
+        }
+        catch (Exception $e)
+        {
+            $output->writeln((string) $e);
+
+            return 1;
+        }
+    }
+
+}
