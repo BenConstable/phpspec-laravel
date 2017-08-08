26dd5c32f973a311ead0d752f732c0635122dea0
Mariusz Piątek <mariusz@tradingdesk.dk>
2017-08-08 15:27:14 +0200

    Added spec

--- spec/PhpSpec/Laravel/Util/LaravelSpec.php
+++ spec/PhpSpec/Laravel/Util/LaravelSpec.php
@@ -17,13 +17,19 @@ class LaravelSpec extends ObjectBehavior
 
     function it_boots_in_the_testing_env_by_default()
     {
-        $this->beConstructedWith(null, '.');
+        $this->beConstructedWith(null, '.', null);
         $this->getEnv()->shouldBe('testing');
     }
 
     function it_allows_the_env_to_be_set_to_anything()
     {
-        $this->beConstructedWith('whatever', '.');
+        $this->beConstructedWith('whatever', '.', null);
         $this->getEnv()->shouldBe('whatever');
+    }
+
+    function it_allows_the_env_file_to_be_set()
+    {
+        $this->beConstructedWith(null, '.', '.phpspec.env');
+        $this->getEnvFile()->shouldBe('.phpspec.env');
     }
 }
